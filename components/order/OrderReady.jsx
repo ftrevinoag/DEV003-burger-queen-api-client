55c791edc8dc0a608c4cccdac714081ceca07a41
Fernanda Treviño <117880355+ftrevinoag@users.noreply.github.com>
2023-05-01 23:14:02 -0600

    agregando comentarios en orderReady

--- components/order/OrderReady.jsx
+++ components/order/OrderReady.jsx
@@ -5,11 +5,16 @@ import Header from '../Header';
 import DeliverSection from './DeliverSection';
 
 const OrderReady = () => {
+  // Estado para almacenar los pedidos en estado de 'delivering'
   const [delivering, setDelivering] = useState([]);
+  // Estado para almacenar los pedidos en estado de 'delivered'
+
   const [delivered, setDelivered] = useState([]);
 
+  // Al montar el componente, se realiza una petición para obtener los pedidos
   useEffect(() => {
     getOrders().then((resp) => {
+      // Se filtran los pedidos por estado
       resp.forEach((order) => {
         if (order.status === 'delivering') setDelivering((prev) => ([...prev, order]));
         else if (order.status === 'delivered') setDelivered((prev) => ([...prev, order]));
